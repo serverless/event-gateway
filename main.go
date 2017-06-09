5ea1438d48c37239f45cdb844eb22c6232d16d0b
Maciej Winnicki <mthenw@users.noreply.github.com>
2017-06-09 13:45:40 +0200

    add request duration histogram. Closes #26 (#31)

--- main.go
+++ main.go
@@ -13,11 +13,22 @@ import (
 	"github.com/serverless/gateway/db"
 	"github.com/serverless/gateway/endpoints"
 	"github.com/serverless/gateway/functions"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+var durationMetric = prometheus.NewHistogram(
+	prometheus.HistogramOpts{
+		Name:    "gateway_request_duration_milliseconds",
+		Help:    "Request duration distribution",
+		Buckets: prometheus.ExponentialBuckets(0.1, 1.1, 140),
+	})
 
 func main() {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
+
+	prometheus.MustRegister(durationMetric)
 
 	dataDir := flag.String("data-dir", "", "Path to a data directory to store instance state.")
 	flag.Parse()
@@ -30,7 +41,6 @@ func main() {
 	defer db.Close()
 
 	router := httprouter.New()
-	router.GET("/status", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {})
 
 	fns := &functions.Functions{
 		DB:        db,
@@ -48,6 +58,8 @@ func main() {
 	ensapi := &endpoints.HTTPAPI{Endpoints: ens}
 	ensapi.RegisterRoutes(router)
 
-	err = http.ListenAndServe(":8080", router)
+	router.GET("/status", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {})
+	router.Handler("GET", "/metrics", prometheus.Handler())
+	err = http.ListenAndServe(":8080", HTTPLogger{router, durationMetric})
 	logger.Fatal("server failed", zap.Error(err))
 }
