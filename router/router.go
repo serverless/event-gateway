6e06252b6047af39ebb9227603b974210bfed6a5
Maciej Winnicki <maciej.winnicki@serverless.com>
2017-07-14 21:57:46 +0200

    add endpoint for emitting events

--- router/router.go
+++ router/router.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -29,7 +28,7 @@ type Router struct {
 	drain                chan struct{}
 	drainWaitGroup       sync.WaitGroup
 	active               bool
-	work                 chan work
+	work                 chan event
 	responseWriteTimeout time.Duration
 }
 
@@ -45,110 +44,44 @@ func New(targetCache targetcache.TargetCache, dropMetric prometheus.Counter, log
 	}
 }
 
-// IsDraining returns true if this Router is being drained of items
-// in its work queue before shutting down.
-func (router *Router) IsDraining() bool {
-	select {
-	case <-router.drain:
-		return true
-	default:
-	}
-	return false
-}
-
 func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// if we're draining requests, spit back a 503
-	if router.IsDraining() {
-		http.Error(w, http.StatusText(503), 503)
+	if router.isDraining() {
+		http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
 	}
-
-	path := strings.TrimPrefix(r.URL.EscapedPath(), "/")
-	id := strings.ToUpper(r.Method) + "-" + path
-	endpointID := endpoints.EndpointID(id)
-	router.log.Debug("router serving request", zap.String("endpoint", string(endpointID)))
 
 	reqBuf, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Fprintf(w, "%s", err)
-	}
-
-	res, err := router.CallEndpoint(endpointID, reqBuf)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	_, err = w.Write(res)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-}
+	topic := r.Header.Get("event")
+	if r.Method == http.MethodPost && topic != "" {
+		router.processEvent(event{
+			topics:  []pubsub.TopicID{pubsub.TopicID(topic)},
+			payload: reqBuf,
+		})
 
-func (router *Router) enqueueWork(topicMap map[pubsub.TopicID]struct{}, payload []byte) {
-	if len(topicMap) == 0 {
-		return
-	}
-	topics := []pubsub.TopicID{}
-	for topic := range topicMap {
-		topics = append(topics, topic)
-	}
-	select {
-	case router.work <- work{
-		topics:  topics,
-		payload: payload,
-	}:
-	default:
-		// We could not submit any work, this is NOT good but
-		// we will sacrifice consistency for availability for now.
-		router.dropMetric.Inc()
-	}
-}
+		w.WriteHeader(http.StatusAccepted)
+	} else {
+		path := strings.TrimPrefix(r.URL.EscapedPath(), "/")
+		id := strings.ToUpper(r.Method) + "-" + path
+		endpointID := endpoints.EndpointID(id)
+		router.log.Debug("router serving request", zap.String("endpoint", string(endpointID)))
 
-// CallEndpoint determines which function to call when an endpoint is hit, and
-// submits pubsub events to the work queue.
-func (router *Router) CallEndpoint(endpointID endpoints.EndpointID, payload []byte) ([]byte, error) {
-	// Figure out what function we're targeting.
-	backingFunction := router.targetCache.BackingFunction(endpointID)
-	if backingFunction == nil {
-		retErr := errors.New("for endpoint ID:" + string(endpointID) + ", could not find backing function")
-		return []byte{}, retErr
-	}
-
-	return router.CallFunction(*backingFunction, payload)
-}
-
-// CallFunction looks up a function and calls it.
-func (router *Router) CallFunction(backingFunctionID functions.FunctionID, payload []byte) ([]byte, error) {
-	backingFunction := router.targetCache.Function(backingFunctionID)
-	if backingFunction == nil {
-		resErr := errors.New("unable to look up backing function: " + string(backingFunctionID))
-		return []byte{}, resErr
-	}
-
-	var chosenFunction = backingFunction.ID
-	if backingFunction.Group != nil {
-		chosen, err := backingFunction.Group.Functions.Choose()
+		res, err := router.callEndpoint(endpointID, reqBuf)
 		if err != nil {
-			return []byte{}, err
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-		chosenFunction = chosen
-	}
 
-	// Call the target backing function.
-	f := router.targetCache.Function(chosenFunction)
-	if f == nil {
-		resErr := errors.New("unable to look up backing function: " + string(chosenFunction))
-		return []byte{}, resErr
+		_, err = w.Write(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
-
-	result, err := f.Call(payload)
-	if err != nil {
-		resErr := errors.New("unable to reach backing function: " + err.Error())
-		return []byte{}, resErr
-	}
-
-	return result, nil
 }
 
 // StartWorkers spins up NWorkers goroutines for processing
@@ -164,63 +97,12 @@ func (router *Router) StartWorkers() {
 	router.active = true
 
 	if router.work == nil {
-		router.work = make(chan work, router.NWorkers*2)
+		router.work = make(chan event, router.NWorkers*2)
 	}
 
 	for i := 0; i < int(router.NWorkers); i++ {
 		router.drainWaitGroup.Add(1)
-		go router.Work()
-	}
-}
-
-// Work is the main loop for a pub/sub worker goroutine
-func (router *Router) Work() {
-	for {
-		// we use three select statements here to give preference
-		// to the work chan, but fall-through to exiting when
-		// the drain chan is closed and there's nothing to do.
-
-		// 1. see if there's work in a non-blocking way
-		select {
-		case work := <-router.work:
-			router.ProcessEvents(work)
-			continue
-		default:
-		}
-
-		// 2. wait on either work or the drain to close,
-		//    blocking on either.
-		select {
-		case <-router.drain:
-			// check AGAIN to make sure there's no work.
-			// without this, there is a race condition
-			// where we exit before work is processed.
-			select {
-			case work := <-router.work:
-				router.ProcessEvents(work)
-				continue
-			default:
-			}
-
-			// no more work to do, decrement WaitGroup and return
-			router.drainWaitGroup.Done()
-			return
-		case work := <-router.work:
-			router.ProcessEvents(work)
-		}
-	}
-}
-
-// ProcessEvents sends events to a set of topics,
-// and for each of the functions that get called
-// as part of those topics, enqueue more work if
-// they are producers for other topics.
-func (router *Router) ProcessEvents(work work) {
-	for _, topicID := range work.topics {
-		subscribers := router.targetCache.SubscribersOfTopic(topicID)
-		for _, subscriber := range subscribers {
-			router.CallFunction(subscriber, work.payload)
-		}
+		go router.loop()
 	}
 }
 
@@ -279,4 +161,130 @@ func (router *Router) WaitForSubscriber(topic pubsub.TopicID) <-chan struct{} {
 		close(updatedChan)
 	}()
 	return updatedChan
+}
+
+// callEndpoint determines which function to call when an endpoint is hit.
+func (router *Router) callEndpoint(endpointID endpoints.EndpointID, payload []byte) ([]byte, error) {
+	// Figure out what function we're targeting.
+	backingFunction := router.targetCache.BackingFunction(endpointID)
+	if backingFunction == nil {
+		retErr := errors.New("for endpoint ID:" + string(endpointID) + ", could not find backing function")
+		return []byte{}, retErr
+	}
+
+	return router.callFunction(*backingFunction, payload)
+}
+
+// callFunction looks up a function and calls it.
+func (router *Router) callFunction(backingFunctionID functions.FunctionID, payload []byte) ([]byte, error) {
+	backingFunction := router.targetCache.Function(backingFunctionID)
+	if backingFunction == nil {
+		resErr := errors.New("unable to look up backing function: " + string(backingFunctionID))
+		return []byte{}, resErr
+	}
+
+	var chosenFunction = backingFunction.ID
+	if backingFunction.Group != nil {
+		chosen, err := backingFunction.Group.Functions.Choose()
+		if err != nil {
+			return []byte{}, err
+		}
+		chosenFunction = chosen
+	}
+
+	// Call the target backing function.
+	f := router.targetCache.Function(chosenFunction)
+	if f == nil {
+		resErr := errors.New("unable to look up backing function: " + string(chosenFunction))
+		return []byte{}, resErr
+	}
+
+	result, err := f.Call(payload)
+	if err != nil {
+		resErr := errors.New("unable to reach backing function: " + err.Error())
+		return []byte{}, resErr
+	}
+
+	return result, nil
+}
+
+// loop is the main loop for a pub/sub worker goroutine
+func (router *Router) loop() {
+	for {
+		// we use three select statements here to give preference
+		// to the work chan, but fall-through to exiting when
+		// the drain chan is closed and there's nothing to do.
+
+		// 1. see if there's work in a non-blocking way
+		select {
+		case e := <-router.work:
+			router.processEvent(e)
+			continue
+		default:
+		}
+
+		// 2. wait on either work or the drain to close,
+		//    blocking on either.
+		select {
+		case <-router.drain:
+			// check AGAIN to make sure there's no work.
+			// without this, there is a race condition
+			// where we exit before work is processed.
+			select {
+			case e := <-router.work:
+				router.processEvent(e)
+				continue
+			default:
+			}
+
+			// no more work to do, decrement WaitGroup and return
+			router.drainWaitGroup.Done()
+			return
+		case e := <-router.work:
+			router.processEvent(e)
+		}
+	}
+}
+
+// processEvent sends event to a set of topics,
+// and for each of the functions that get called
+// as part of those topics.
+func (router *Router) processEvent(e event) {
+	for _, topicID := range e.topics {
+		subscribers := router.targetCache.SubscribersOfTopic(topicID)
+		for _, subscriber := range subscribers {
+			router.callFunction(subscriber, e.payload)
+		}
+	}
+}
+
+func (router *Router) enqueueWork(topicMap map[pubsub.TopicID]struct{}, payload []byte) {
+	if len(topicMap) == 0 {
+		return
+	}
+	topics := []pubsub.TopicID{}
+	for topic := range topicMap {
+		topics = append(topics, topic)
+	}
+	select {
+	case router.work <- event{
+		topics:  topics,
+		payload: payload,
+	}:
+	default:
+		// We could not submit any work, this is NOT good but
+		// we will sacrifice consistency for availability for now.
+		router.dropMetric.Inc()
+	}
+}
+
+// isDraining returns true if this Router is being drained of items
+// in its work queue before shutting down.
+func (router *Router) isDraining() bool {
+	select {
+	case <-router.drain:
+		return true
+	default:
+	}
+	return false
 }
