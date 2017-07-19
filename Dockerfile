324a0e099161ffd3c5216dafb9bc48b8613372f0
Maciej Winnicki <maciej.winnicki@serverless.com>
2017-07-19 16:44:45 +0200

    remove deplyoment info. Closes #134

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM golang:1.8.1-alpine
-
-ADD ./bin/gateway /usr/bin/gateway
-ENTRYPOINT ["/usr/bin/gateway", "-embed-master", "-embed-cli-addr=http://0.0.0.0:2379"]
--- _infrastructure/env-dev/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.9.5"
-
-  backend "s3" {
-    bucket = "serverlessdev-tfstate"
-    region = "us-east-1"
-    key    = "gateway/env-dev.tfstate"
-  }
-}
