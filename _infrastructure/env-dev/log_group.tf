2ac12080ea2cd2a11d937eefb955181773dbeb72
Maciej Winnicki <mthenw@users.noreply.github.com>
2017-07-20 11:03:08 +0200

    remove deplyoment info. Closes #134 (#135)

--- _infrastructure/env-dev/log_group.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "gateway" {
-  name              = "${var.name}"
-  retention_in_days = 30
-}
--- _infrastructure/env-dev/state.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "infrastructure" {
-  backend = "s3"
-
-  config {
-    bucket = "serverlessdev-tfstate"
-    key    = "infrastructure/env-dev.tfstate"
-    region = "${var.aws_region}"
-  }
-}
