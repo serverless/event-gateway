824264babb3d7ebae03dd7bbe6e81c9e13e63cc8
Maciej Winnicki <maciej.winnicki@serverless.com>
2017-08-09 21:46:41 +0200

    fix info

--- cmd/event-gateway/main.go
+++ cmd/event-gateway/main.go
@@ -31,7 +31,7 @@ func main() {
 	logLevel := zap.LevelFlag("log-level", zap.InfoLevel, `The level of logging to show after the event gateway has started. The available log levels are "debug", "info", "warn", and "err".`)
 	logFormat := flag.String("log-format", "", `The format of logs. The available formats are "text", "json".)`)
 	dbHosts := flag.String("db-hosts", "127.0.0.1:2379", "Comma-separated list of database hosts to connect to.")
-	developmentMode := flag.Bool("dev", false, `Run in development mode with embedded etcd and "console" log format.`)
+	developmentMode := flag.Bool("dev", false, `Run in development mode with embedded etcd and "text" log format.`)
 	embedPeerAddr := flag.String("embed-peer-addr", "http://127.0.0.1:2380", "Address for testing embedded etcd to receive peer connections.")
 	embedCliAddr := flag.String("embed-cli-addr", "http://127.0.0.1:2379", "Address for testing embedded etcd to receive client connections.")
 	embedDataDir := flag.String("embed-data-dir", "default.etcd", "Path for testing embedded etcd to store its state.")
