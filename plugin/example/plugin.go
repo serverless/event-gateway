f354244ee52cb149d44f1bdec6c4d73dd7f0c923
Maciej Winnicki <maciej.winnicki@serverless.com>
2017-09-29 10:12:48 +0200

    fix lint errors in example plugin

--- plugin/example/plugin.go
+++ plugin/example/plugin.go
@@ -7,17 +7,20 @@ import (
 	"github.com/serverless/event-gateway/plugin"
 )
 
+// Simple plugin demonstrating how to build plugins.
 type Simple struct{}
 
+// Subscriptions return list of events that plugin listens to.
 func (s *Simple) Subscriptions() []plugin.Subscription {
 	return []plugin.Subscription{
 		plugin.Subscription{
-			EventType: event.Type("user.created"),
+			EventType: event.Type("gateway.event.received"),
 			Type:      plugin.Async,
 		},
 	}
 }
 
+// React is called for every event that plugin subscribed to.
 func (s *Simple) React(event event.Event) error {
 	return errors.New("error on reacting")
 }
