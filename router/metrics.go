935302b2bd63e2e88eb04545b0108cd841c5bfd7
Maciej Winnicki <maciej.winnicki@serverless.com>
2017-11-09 15:02:28 +0100

    make metrics name consistent

--- router/metrics.go
+++ router/metrics.go
@@ -20,40 +20,40 @@ func init() {
 var routerEventsAsyncReceived = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Namespace: "gateway",
-		Subsystem: "router",
-		Name:      "events_async_received_total",
+		Subsystem: "events",
+		Name:      "async_received_total",
 		Help:      "Total of asynchronously handled events received (including system events).",
 	})
 
 var routerEventsAsyncDropped = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Namespace: "gateway",
-		Subsystem: "router",
-		Name:      "events_async_dropped_total",
+		Subsystem: "events",
+		Name:      "async_dropped_total",
 		Help:      "Total of asynchronously handled events dropped due to insufficient processing power.",
 	})
 
 var routerEventsAsyncProceeded = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Namespace: "gateway",
-		Subsystem: "router",
-		Name:      "events_async_proceeded_total",
+		Subsystem: "events",
+		Name:      "async_proceeded_total",
 		Help:      "Total of asynchronously proceeded events.",
 	})
 
 var routerEventsSyncReceived = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Namespace: "gateway",
-		Subsystem: "router",
-		Name:      "events_sync_received_total",
+		Subsystem: "events",
+		Name:      "sync_received_total",
 		Help:      "Total of synchronously handled (HTTP and invoke) events received.",
 	})
 
 var routerEventsSyncProceeded = prometheus.NewCounter(
 	prometheus.CounterOpts{
 		Namespace: "gateway",
-		Subsystem: "router",
-		Name:      "events_sync_proceeded_total",
+		Subsystem: "events",
+		Name:      "sync_proceeded_total",
 		Help: "Total of synchronously proceeded events. This counter excludes events for which there was no function " +
 			"registered or error occured during processing phase.",
 	})
@@ -61,17 +61,17 @@ var routerEventsSyncProceeded = prometheus.NewCounter(
 var routerBacklog = prometheus.NewGauge(
 	prometheus.GaugeOpts{
 		Namespace: "gateway",
-		Subsystem: "router",
-		Name:      "backlog_events",
-		Help:      "Gauge of asynchronous events count waiting to be processed by the router.",
+		Subsystem: "events",
+		Name:      "backlog",
+		Help:      "Gauge of asynchronous events count waiting to be processed.",
 	})
 
 var routerProcessingDuration = prometheus.NewHistogram(
 	prometheus.HistogramOpts{
 		Namespace: "gateway",
-		Subsystem: "router",
-		Name:      "event_processing_seconds",
-		Help: "Bucketed histogram of processing duration of an event in the router. " +
+		Subsystem: "events",
+		Name:      "async_processing_seconds",
+		Help: "Bucketed histogram of processing duration of an event. " +
 			"From receiving the asynchronous event to calling a function.",
 		Buckets: prometheus.ExponentialBuckets(0.00001, 2, 20),
 	})
