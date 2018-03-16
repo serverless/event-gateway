a81a51aaaa19fd2387397b7b974c86c09eb58a26
Maciej Winnicki <maciej.winnicki@serverless.com>
2018-03-16 21:30:15 +0100

    add Call test for HTTP provider

--- providers/http/http_test.go
+++ providers/http/http_test.go
@@ -1,15 +1,48 @@
 package http_test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/serverless/event-gateway/function"
-	"github.com/serverless/event-gateway/providers/http"
+	httpprovider "github.com/serverless/event-gateway/providers/http"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCall(t *testing.T) {
+	echo := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		payload, _ := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		fmt.Fprint(w, string(payload))
+	}))
+	provider := httpprovider.HTTP{
+		URL: echo.URL,
+	}
+
+	resp, err := provider.Call([]byte("hello"))
+
+	assert.Nil(t, err)
+	assert.Equal(t, "hello", string(resp))
+}
+
+func TestCall_InternalError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	provider := httpprovider.HTTP{
+		URL: ts.URL,
+	}
+
+	_, err := provider.Call([]byte("hello"))
+
+	assert.EqualError(t, err, "Function call failed because of runtime error. Error: \"HTTP status code: 500\"")
+}
+
 func TestValidate_MissingURL(t *testing.T) {
-	provider := http.HTTP{}
+	provider := httpprovider.HTTP{}
 
 	err := provider.Validate()
 
