53174bfea1c143747b1f863bdef0a74b7c6de6b8
Maciej Winnicki <maciej.winnicki@serverless.com>
2018-03-23 21:31:51 +0100

    better coverage for AWS Lambda

--- providers/awslambda/awslambda_test.go
+++ providers/awslambda/awslambda_test.go
@@ -1,64 +1,148 @@
 package awslambda_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/golang/mock/gomock"
+	"github.com/serverless/event-gateway/function"
 	"github.com/serverless/event-gateway/providers/awslambda"
 	"github.com/serverless/event-gateway/providers/awslambda/mock"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap/zapcore"
 )
 
-func TestLoad_MalformedJSON(t *testing.T) {
-	config := []byte(`{"arn": "", "region": `)
-	loader := awslambda.ProviderLoader{}
+func TestLoad(t *testing.T) {
+	for _, testCase := range loadTests {
+		config := []byte(testCase.config)
+		loader := awslambda.ProviderLoader{}
 
-	provider, err := loader.Load(config)
+		_, err := loader.Load(config)
 
-	assert.Nil(t, provider)
-	assert.EqualError(t, err, "unable to load function provider config: unexpected end of JSON input")
-}
-
-func TestLoad_MissingARN(t *testing.T) {
-	config := []byte(`{"arn": "", "region": "us-east-1"}`)
-	loader := awslambda.ProviderLoader{}
-
-	provider, err := loader.Load(config)
-
-	assert.Nil(t, provider)
-	assert.EqualError(t, err, "missing required fields for AWS Lambda function")
-}
-
-func TestLoad_MissingRegion(t *testing.T) {
-	config := []byte(`{"arn": "test", "region": ""}`)
-	loader := awslambda.ProviderLoader{}
-
-	provider, err := loader.Load(config)
-
-	assert.Nil(t, provider)
-	assert.EqualError(t, err, "missing required fields for AWS Lambda function")
+		assert.Equal(t, testCase.expectedError, err)
+	}
 }
 
 func TestCall(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	serviceMock := mock.NewMockLambdaAPI(mockCtrl)
-	opts := &lambda.InvokeInput{
-		FunctionName: aws.String("testarn"),
-		Payload:      []byte("testpayload"),
+	for _, testCase := range callTests {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		serviceMock := mock.NewMockLambdaAPI(mockCtrl)
+		opts := &lambda.InvokeInput{
+			FunctionName: aws.String("testarn"),
+			Payload:      []byte("testpayload"),
+		}
+		serviceMock.EXPECT().Invoke(opts).Return(testCase.invokeResult, testCase.invokeError)
+
+		provider := awslambda.AWSLambda{
+			Service: serviceMock,
+
+			ARN:    "testarn",
+			Region: "us-east-1",
+		}
+
+		output, err := provider.Call([]byte("testpayload"))
+
+		assert.Equal(t, testCase.expectedResult, output)
+		assert.Equal(t, testCase.expectedError, err)
 	}
-	serviceMock.EXPECT().Invoke(opts).Return(&lambda.InvokeOutput{Payload: []byte("testoutput")}, nil)
-	provider := awslambda.AWSLambda{
-		Service: serviceMock,
+}
 
-		ARN:    "testarn",
-		Region: "us-east-1",
+func TestMarshalLogObject(t *testing.T) {
+	for _, testCase := range logTests {
+		enc := zapcore.NewMapObjectEncoder()
+
+		testCase.provider.MarshalLogObject(enc)
+
+		assert.Equal(t, testCase.expectedFields, enc.Fields)
 	}
+}
 
-	output, err := provider.Call([]byte("testpayload"))
+var loadTests = []struct {
+	config        string
+	expectedError error
+}{
+	{
+		`{"arn": "", "region": `,
+		errors.New("unable to load function provider config: unexpected end of JSON input"),
+	},
+	{
+		`{"arn": "", "region": "us-east-1"}`,
+		errors.New("missing required fields for AWS Lambda function"),
+	},
+	{
+		`{"arn": "test", "region": ""}`,
+		errors.New("missing required fields for AWS Lambda function"),
+	},
+}
 
-	assert.Nil(t, err)
-	assert.Equal(t, []byte("testoutput"), output)
+var callTests = []struct {
+	invokeResult   *lambda.InvokeOutput
+	invokeError    error
+	expectedResult []byte
+	expectedError  error
+}{
+	{
+		&lambda.InvokeOutput{Payload: []byte("testres")},
+		nil,
+		[]byte("testres"),
+		nil,
+	},
+	{
+		&lambda.InvokeOutput{FunctionError: aws.String("FuncErr")},
+		nil,
+		[]byte(nil),
+		&function.ErrFunctionError{Original: errors.New("FuncErr")},
+	},
+	{
+		nil,
+		awserr.New("TestCode", "", nil),
+		[]byte(nil),
+		&function.ErrFunctionCallFailed{Original: awserr.New("TestCode", "", nil)},
+	},
+	{
+		nil,
+		awserr.New("AccessDeniedException", "", nil),
+		[]byte(nil),
+		&function.ErrFunctionAccessDenied{Original: awserr.New("AccessDeniedException", "", nil)},
+	},
+	{
+		nil,
+		awserr.New(lambda.ErrCodeServiceException, "", nil),
+		[]byte(nil),
+		&function.ErrFunctionProviderError{Original: awserr.New(lambda.ErrCodeServiceException, "", nil)},
+	},
+}
+
+var logTests = []struct {
+	provider       function.Provider
+	expectedFields map[string]interface{}
+}{
+	{
+		awslambda.AWSLambda{
+			ARN:    "test",
+			Region: "us-east-1",
+		},
+		map[string]interface{}{
+			"arn":    "test",
+			"region": "us-east-1",
+		},
+	},
+	{
+		awslambda.AWSLambda{
+			AWSAccessKeyID:     "id",
+			AWSSecretAccessKey: "key",
+			AWSSessionToken:    "token",
+		},
+		map[string]interface{}{
+			"arn":                "",
+			"region":             "",
+			"awsAccessKeyId":     "*****",
+			"awsSecretAccessKey": "*****",
+			"awsSessionToken":    "*****",
+		},
+	},
 }
