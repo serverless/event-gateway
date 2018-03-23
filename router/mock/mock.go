1e761d7de8b9cdeaffe31f03046561dd04ca92ec
Raees Iqbal <10067728+elsteelbrain@users.noreply.github.com>
2018-03-23 13:28:48 +0400

    Add Provider mock

--- router/mock/mock.go
+++ router/mock/mock.go
@@ -1,3 +1,4 @@
 //go:generate mockgen -package mock -destination ./targetcache.go github.com/serverless/event-gateway/router Targeter
+//go:generate mockgen -package mock -destination ./provider.go github.com/serverless/event-gateway/function Provider
 
 package mock
