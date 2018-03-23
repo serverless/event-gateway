3c0374a915d5e99aea590e86b50633e7ba0e26f3
Maciej Winnicki <mthenw@users.noreply.github.com>
2018-03-23 22:16:31 +0100

    Improve Provider tests (#397)
    * Add better tests for providers
    
    * better coverage for AWS Lambda

--- providers/awskinesis/awskinesis_test.go
+++ providers/awskinesis/awskinesis_test.go
@@ -1,59 +1,125 @@
 package awskinesis_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/kinesis"
 	"github.com/golang/mock/gomock"
+	"github.com/serverless/event-gateway/function"
 	"github.com/serverless/event-gateway/providers/awskinesis"
 	"github.com/serverless/event-gateway/providers/awskinesis/mock"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap/zapcore"
 )
 
-func TestLoad_MalformedJSON(t *testing.T) {
-	config := []byte(`{"streamName": "", "region": `)
-	loader := awskinesis.ProviderLoader{}
+func TestLoad(t *testing.T) {
+	for _, testCase := range loadTests {
+		config := []byte(testCase.config)
+		loader := awskinesis.ProviderLoader{}
 
-	provider, err := loader.Load(config)
+		_, err := loader.Load(config)
 
-	assert.Nil(t, provider)
-	assert.EqualError(t, err, "unable to load function provider config: unexpected end of JSON input")
-}
-
-func TestLoad_MissingStreamName(t *testing.T) {
-	config := []byte(`{"streamName": "", "region": "us-east-1"}`)
-	loader := awskinesis.ProviderLoader{}
-
-	provider, err := loader.Load(config)
-
-	assert.Nil(t, provider)
-	assert.EqualError(t, err, "missing required fields for AWS Kinesis function")
-}
-
-func TestLoad_MissingRegion(t *testing.T) {
-	config := []byte(`{"streamName": "test", "region": ""}`)
-	loader := awskinesis.ProviderLoader{}
-
-	provider, err := loader.Load(config)
-
-	assert.Nil(t, provider)
-	assert.EqualError(t, err, "missing required fields for AWS Kinesis function")
+		assert.Equal(t, testCase.expectedError, err)
+	}
 }
 
 func TestCall(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	serviceMock := mock.NewMockKinesisAPI(mockCtrl)
-	serviceMock.EXPECT().PutRecord(gomock.Any()).Return(&kinesis.PutRecordOutput{SequenceNumber: aws.String("testseq")}, nil)
-	provider := awskinesis.AWSKinesis{
-		Service:    serviceMock,
-		StreamName: "teststream",
-		Region:     "us-east-1",
+	for _, testCase := range callTests {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		serviceMock := mock.NewMockKinesisAPI(mockCtrl)
+		serviceMock.EXPECT().PutRecord(gomock.Any()).Return(testCase.putRecordResult, testCase.putRecordError)
+
+		provider := awskinesis.AWSKinesis{
+			Service:    serviceMock,
+			StreamName: "teststream",
+			Region:     "us-east-1",
+		}
+
+		output, err := provider.Call([]byte("testpayload"))
+
+		assert.Equal(t, testCase.expectedResult, output)
+		assert.Equal(t, testCase.expectedError, err)
 	}
+}
 
-	output, err := provider.Call([]byte("testpayload"))
+func TestMarshalLogObject(t *testing.T) {
+	for _, testCase := range logTests {
+		enc := zapcore.NewMapObjectEncoder()
 
-	assert.Nil(t, err)
-	assert.Equal(t, []byte("testseq"), output)
+		testCase.provider.MarshalLogObject(enc)
+
+		assert.Equal(t, testCase.expectedFields, enc.Fields)
+	}
+}
+
+var loadTests = []struct {
+	config        string
+	expectedError error
+}{
+	{
+		`{"streamName": "", "region": `,
+		errors.New("unable to load function provider config: unexpected end of JSON input"),
+	},
+	{
+		`{"streamName": "", "region": "us-east-1"}`,
+		errors.New("missing required fields for AWS Kinesis function"),
+	},
+	{
+		`{"streamName": "test", "region": ""}`,
+		errors.New("missing required fields for AWS Kinesis function"),
+	},
+}
+
+var callTests = []struct {
+	putRecordResult *kinesis.PutRecordOutput
+	putRecordError  error
+	expectedResult  []byte
+	expectedError   error
+}{
+	{
+		&kinesis.PutRecordOutput{SequenceNumber: aws.String("testseq")},
+		nil,
+		[]byte("testseq"),
+		nil,
+	},
+	{
+		nil,
+		awserr.New("", "", nil),
+		[]byte(nil),
+		&function.ErrFunctionCallFailed{Original: awserr.New("", "", nil)},
+	},
+}
+
+var logTests = []struct {
+	provider       function.Provider
+	expectedFields map[string]interface{}
+}{
+	{
+		awskinesis.AWSKinesis{
+			StreamName: "test",
+			Region:     "us-east-1",
+		},
+		map[string]interface{}{
+			"streamName": "test",
+			"region":     "us-east-1",
+		},
+	},
+	{
+		awskinesis.AWSKinesis{
+			AWSAccessKeyID:     "id",
+			AWSSecretAccessKey: "key",
+			AWSSessionToken:    "token",
+		},
+		map[string]interface{}{
+			"streamName":         "",
+			"region":             "",
+			"awsAccessKeyId":     "*****",
+			"awsSecretAccessKey": "*****",
+			"awsSessionToken":    "*****",
+		},
+	},
 }
