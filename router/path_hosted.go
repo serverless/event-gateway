cac8db6176066465e67496ed9f4a34d899618cd2
Sebastian Borza <sebito91@gmail.com>
2018-07-24 10:48:06 -0500

    adding in proper router processing

--- router/path_hosted.go
+++ router/path_hosted.go
@@ -3,20 +3,25 @@
 package router
 
 import (
+	"regexp"
 	"strings"
 )
 
-// extractPath extracts path from hosted EG host name (<space>.eventgateway([a-z-]*)?.io|slsgateway.com)
+var hostedDomainPattern *regexp.Regexp
+
+func init() {
+	hostedDomainPattern = regexp.MustCompile("(eventgateway([a-z-]*)?.io|slsgateway.com)")
+}
+
 func extractPath(host, path string) string {
-	subdomain := strings.Split(host, ".")[0]
-	return basePath + subdomain + path
+	extracted := path
+	if hostedDomainPattern.Copy().MatchString(host) {
+		subdomain := strings.Split(host, ".")[0]
+		extracted = basePath + subdomain + path
+	}
+	return extracted
 }
 
-func systemPathFromSpace(space string) string {
-	return basePath + space + "/"
-}
-
-// systemPathFromPath constructs path from path on which event was emitted. Helpful for "event.received" system event.
-func systemPathFromPath(path string) string {
-	return basePath + strings.Split(path, "/")[1] + "/"
+func systemEventPath(space string) string {
+	return basePath + space + basePath
 }
