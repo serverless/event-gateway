4b4226b23d45c7580c00f3b7bc07d4ca6c89fbbf
Maciej Winnicki <maciej.winnicki@serverless.com>
2018-09-05 09:44:46 +0200

    use vendor with modules

--- codecov.sh
+++ codecov.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+    go test -race -mod=vendor -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
@@ -12,6 +12,6 @@ for d in $(go list ./... | grep -v vendor); do
 done
 
 # include coverage for hosted EG
-go test -race -coverprofile=profile.out -covermode=atomic -tags=hosted ./router
+go test -race -mod=vendor -coverprofile=profile.out -covermode=atomic -tags=hosted ./router
 cat profile.out >> coverage.txt
 rm profile.out
